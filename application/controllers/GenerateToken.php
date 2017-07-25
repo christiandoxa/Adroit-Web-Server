c361616aecd77b3f92d9bab3782380c356e2adf8
Yobel Galih <yobelchris@gmail.com>
2017-07-25 09:43:39 +0700

    minor fix

--- application/controllers/GenerateToken.php
+++ application/controllers/GenerateToken.php
@@ -2,45 +2,38 @@
 
 class GenerateToken
 {
-    public $hashfin = "";
-
-    public function __construct($username, $password)
-    {
-        $token = "";
-        $token2 = "";
-        $codeAlphabet = "abcdefghijklmnopqrstuvwxyz";
-        $codeAlphabet .= "0123456789";
-        $max = strlen($codeAlphabet); // edited
-
-        for ($i = 0; $i < rand(12, 32); $i++) {
-            $token .= $codeAlphabet[$this->crypto_rand_secure(0, $max - 1)];
-        }
-
-        for ($i = 0; $i < rand(3, 7); $i++) {
-            $token2 .= $codeAlphabet[$this->crypto_rand_secure(0, $max - 1)];
-        }
-
-        $hash = $token2 . "?" . hash("sha256", $password . $username, false) . "!" . $token;
-        $GLOBALS['hashfin'] = $hash;
-    }
-
-    function crypto_rand_secure($min, $max)
+    function rand_alphnum($min, $max)
     {
         $range = $max - $min;
-        if ($range < 1) return $min; // not so random...
+        if ($range < 1) return $min;
         $log = ceil(log($range, 2));
-        $bytes = (int)($log / 8) + 1; // length in bytes
-        $bits = (int)$log + 1; // length in bits
-        $filter = (int)(1 << $bits) - 1; // set all lower bits to 1
+        $bytes = (int)($log / 8) + 1;
+        $bits = (int)$log + 1;
+        $filter = (int)(1 << $bits) - 1;
         do {
             $rnd = hexdec(bin2hex(openssl_random_pseudo_bytes($bytes)));
-            $rnd = $rnd & $filter; // discard irrelevant bits
+            $rnd = $rnd & $filter;
         } while ($rnd > $range);
         return $min + $rnd;
     }
 
-    public function getHash()
+    public function getHash($username, $password)
     {
-        return $GLOBALS['hashfin'];
+        $randnumb = "";
+        $randnumb2 = "";
+        $codeAlphabet = "abcdefghijklmnopqrstuvwxyz";
+        $codeAlphabet .= "0123456789";
+        $max = strlen($codeAlphabet);
+
+        for ($i = 0; $i < rand(12, 32); $i++) {
+            $randnumb .= $codeAlphabet[$this->rand_alphnum(0, $max - 1)];
+        }
+
+        for ($i = 0; $i < rand(3, 7); $i++) {
+            $randnumb2 .= $codeAlphabet[$this->rand_alphnum(0, $max - 1)];
+        }
+
+        $hash = $randnumb2 . "?" . hash("sha256", $password . $username, false) . "!" . $randnumb;
+        return $hash;
     }
 }
